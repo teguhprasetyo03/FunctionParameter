b715db492166748449ef3d7b784523e9b183b842
Teguh Prasetyo <teguhprasetyo261211@gmail.com>
2021-03-19 21:28:31 +0700

    mengubah isi file

--- bin/FunctionParameter.dart
+++ bin/FunctionParameter.dart
@@ -1,14 +1,15 @@
 void main(){
-    SayHi(20,"Andi");
-    HitungLuas(20, 25);
-    SayHi(17, 'Alfian');
+    SayHi('Andi', 20);
+    SayHi('Budi', 21);
+    SayHi('Jono', 22);
+    HitungLuas(20, 20);
 }
 
-void SayHi(int age, String name){
-    print('Hello My Name $name, My Age $age');
+void SayHi(String name, int age){
+    print('Hello My Name is $name, My age $age');
 }
 
 void HitungLuas(int panjang, int lebar){
-    var luas = panjang * lebar;
+    int luas = panjang*lebar;
     print('Luasnya adalah = $luas');
 }
